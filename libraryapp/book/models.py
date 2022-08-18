98ba5219ebebab25403c789069d0cfecbeda4386
VAOkolo <vincentaokolo@outlook.com>
2022-08-18 12:42:18 +0100

    1. getting started, 2. add database and models

--- libraryapp/book/models.py
+++ libraryapp/book/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    author = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
+    
