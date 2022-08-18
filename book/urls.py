7a6f252643c66bca63e4358e7bfd3809bd932b90
eheath30 <elliot.heath@btinternet.com>
2022-08-18 11:49:40 +0100

    deleting

--- book/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('books/',views.home, name="books-name"),
-    path('books/<int:id>',views.show, name="single-book")
-]
-
